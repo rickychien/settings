6740a8c75b934acb345fb39aacaa499020f9082b
Ricky Chien <ricky060709@gmail.com>
2015-05-28 11:31:22 +0800

    Add install.sh for easy setup settings

---
+++ install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# install neobundle first if it is inexistent
+if [ ! -d ~/.vim/bundle/.neobundle ]; then
+  echo "Install NeoBundle..."
+  curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+fi
+
+echo "Link setting scripts to user home directory"
+
+BASEDIR=$(dirname $0)
+SCRIPTS=('.gitconfig' '.hgrc' '.vimrc' '.zshrc')
+
+for script in ${SCRIPTS[@]}
+do
+  echo "link $BASEDIR/$script to $HOME"
+  ln -s -f $BASEDIR/$script $HOME
+done
+
+exit 0
