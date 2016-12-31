250bdfe5f5ec037f2b4e5c2ed18985ade583d5c2
Ricky Chien <ricky060709@gmail.com>
2016-12-31 09:13:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Link setting scripts to user home directory"
 
 echo $(dirname $0)
 BASEDIR="$(cd "$(dirname "$0")" && pwd)"
-SCRIPTS=('.gitconfig' '.hgrc' '.vimrc' '.zshrc')
+SCRIPTS=('.gitconfig' '.gitignore' '.hgrc' '.vimrc' '.zshrc')
 
 for script in ${SCRIPTS[@]}
 do
