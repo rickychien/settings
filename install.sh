98989f6e9983e641afd38ca827415f7f1ad0e7a7
Ricky Chien <ricky060709@gmail.com>
2018-01-21 21:39:51 +0800

    Update settings

--- install.sh
+++ install.sh
@@ -1,14 +1,26 @@
 #! /bin/bash
 
-echo "Link setting scripts to user home directory"
 
-echo $(dirname $0)
+if [ "$(echo $SHELL)" != "/bin/zsh" ]
+then
+  echo "Switch to zsh as default shell"
+  chsh -s /bin/zsh
+fi
+
+if [ ! -d ~/.oh-my-zsh ]
+then
+  echo "Install oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+echo "Create soft link from setting scripts to user home directory"
+
 BASEDIR="$(cd "$(dirname "$0")" && pwd)"
-SCRIPTS=('.gitconfig' '.gitignore', '.vimrc' '.zshrc')
+SCRIPTS=('.gitconfig' '.gitignore' '.vimrc' '.zshrc')
 
 for script in ${SCRIPTS[@]}
 do
-  echo "link $BASEDIR/$script to $HOME"
+  echo " * link $BASEDIR/$script to $HOME"
   ln -sf $BASEDIR/$script $HOME
 done
 
